21eb290a14663d55c36730bc36dab17eb89c0174
Eran Mani <eranmanim@gmail.com>
2021-08-20 23:38:45 +0300

    Added effect when giant moss is walking Saved prefabs

---
+++ Assets/Scripts/Enemies/MossGiantWalkEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiantWalkEffect : MonoBehaviour
+{
+    [SerializeField] GameObject walkImpactEffectPrefab;
+    [SerializeField] Transform effectPos;
+
+    public void WalkEffect()
+    {
+        ParticleSystem effect = Instantiate(walkImpactEffectPrefab, effectPos.position, Quaternion.Euler(83,0,0)).GetComponent<ParticleSystem>();
+        effect.Play();
+    }
+}
