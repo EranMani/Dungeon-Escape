55765c4f223c860d26dc86be4c0764d80c5e81e7
Eran Mani <eranmanim@gmail.com>
2021-08-21 14:12:59 +0300

    Created transition from main menu to gameplay using timeline and code

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public CanvasGroup transitionText;
+    public Image myPanel;
+    float fadeTime = 50f;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(FadeAndLoadRoutine());
+   
     }
 
     public void QuitApp()
     {
         Application.Quit();
+    }
+    
+    private IEnumerator FadeAndLoadRoutine()
+    {
+        myPanel.CrossFadeAlpha(255f, fadeTime, false);
+        yield return new WaitForSeconds(2f);
+
+        float progress = 0;
+        while (transitionText.alpha < 1)
+        {
+            transitionText.alpha = Mathf.Lerp(0f, 1f, progress);
+            progress += 0.01f;
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds(4f);
+
+        SceneManager.LoadScene(1);
     }
 }
